7b8b2c9c09d00e9270b8ebabdcded2734c3c0d0e
Ankita Sood <ankitasood2000@gmail.com>
2025-06-20 23:03:36 +0530

    Added styling for search box

--- src/components/Head.js
+++ src/components/Head.js
@@ -14,7 +14,7 @@ function Head() {
 
   const fetchDataFromAPI = async () => {
     const data = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=14&q=${userSearch}&key=${GOOGLE_API_KEY}`
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=14&type=video&q=${userSearch}&key=${GOOGLE_API_KEY}`
     );
     const dataToJson = await data.json();
     setSearchResult(dataToJson.items);
@@ -28,8 +28,8 @@ function Head() {
   );
 
   return (
-    <>
-      <div className="flex justify-between items-center">
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex justify-between items-center w-full px-4 py-2">
         <div className="flex items-center">
           <button>
             <img
@@ -45,13 +45,12 @@ function Head() {
             className="h-14 px-2"
           />
         </div>
-        <div className="flex items-center">
+
+        <div className="relative flex items-center w-[40rem]">
           <input
             type="text"
-            name=""
-            id=""
             placeholder="Search"
-            className="border rounded-full-lg w-96 p-2 h-10"
+            className="border rounded-l-full w-full p-2 h-10"
             value={userSearch}
             onChange={(e) => setUserSearch(e.target.value)}
           />
@@ -59,10 +58,24 @@ function Head() {
             <img
               src="https://friconix.com/png/fi-xtluhl-magnifying-glass-thin.png"
               alt=""
-              className="h-10 bg-gray-100 rounded-full-lg hover:bg-gray-200"
+              className="h-10 bg-gray-100 rounded-r-full hover:bg-gray-200"
             />
           </button>
+
+          {/* Overlapping dropdown */}
+          <div className="absolute top-12 left-0 bg-white w-full max-h-64 overflow-y-auto shadow-lg rounded-md z-50">
+            {searchResult &&
+              searchResult.map((item) => (
+                <li
+                  key={item.etag}
+                  className="p-2 border-b hover:bg-gray-100 text-sm cursor-pointer"
+                >
+                  {item.snippet.title}
+                </li>
+              ))}
+          </div>
         </div>
+
         <div className="flex items-center">
           <img
             src="https://waryhub.com/files/preview/960x960/11747654922nffoetctd3yijqonmiuoax0gpsrksemmbi8bmilcj65dubpbztzftchhcuwihpvdhj6ucshizdxnuosh1pqhz5j3xpw2ebgmgiac.png"
@@ -76,14 +89,7 @@ function Head() {
           />
         </div>
       </div>
-      <div>
-        {searchResult.length > 0 &&
-          searchResult.map((item) => {
-            // return <li>{item.snippet.title}</li>;
-            console.log(item);
-          })}
-      </div>
-    </>
+    </div>
   );
 }
 
